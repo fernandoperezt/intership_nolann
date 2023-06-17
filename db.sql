6a1c321cd1d3d542fd3479f4435dcb8a9649f7c8
Nolann <92048864+Portal79@users.noreply.github.com>
2023-06-17 20:37:13 +0200

    Update db.sql

--- db.sql
+++ db.sql
@@ -2,6 +2,6 @@ create database if not exists learningAux_db;
 
 use learningAux_db;
 
-source ./Downloads/sql_script/database_creation_script.sql;
+source ./database_creation_script.sql;
 
-source ./Downloads/sql_script/database_fill_script.sql;
+source ./database_fill_script.sql;
